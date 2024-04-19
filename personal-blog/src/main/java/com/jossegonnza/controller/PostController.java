4a625d7b0791c1e7fa7805798c7c95857cfd6b36
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-19 20:19:41 +0100

    Add Create Post

--- personal-blog/src/main/java/com/jossegonnza/controller/PostController.java
+++ personal-blog/src/main/java/com/jossegonnza/controller/PostController.java
@@ -1,11 +1,19 @@
 package com.jossegonnza.controller;
 
+import com.jossegonnza.entity.PostEntity;
+import com.jossegonnza.entity.UserEntity;
 import com.jossegonnza.service.PostService;
+import com.jossegonnza.service.UserService;
+import com.jossegonnza.service.UserServiceImpl;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/post")
@@ -14,9 +22,29 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/home") //EndPoint que nos retornará una vista con el método para ver todos los posts
     public String homePage(Model model){
         model.addAttribute("posts", postService.getAllPost());
         return "/posts/home";
     }
+
+    @GetMapping("/new")
+    public String newPostPage() {
+        return "/posts/create-post";
+    }
+
+    @PostMapping("/create")
+    public String createPost(PostEntity post, HttpSession session) {
+        post.setCreatedAt(LocalDateTime.now());
+
+        UserEntity user = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString())).get();
+        post.setUser(user);
+
+        postService.createPost(post);
+        return "redirect:/post/home";
+    }
+
 }
