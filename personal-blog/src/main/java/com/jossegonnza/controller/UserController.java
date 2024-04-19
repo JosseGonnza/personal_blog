10c52a942be2ffbb1396443aa18863079227dff1
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-19 17:44:22 +0100

    Add Logout

--- personal-blog/src/main/java/com/jossegonnza/controller/UserController.java
+++ personal-blog/src/main/java/com/jossegonnza/controller/UserController.java
@@ -2,12 +2,16 @@ package com.jossegonnza.controller;
 
 import com.jossegonnza.entity.UserEntity;
 import com.jossegonnza.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -34,6 +38,28 @@ public class UserController {
     @GetMapping(value = {"/login", "/"})
     public String loginPage() {
         return "/users/login";
+    }
+
+    @GetMapping("/access")
+    public String access(HttpSession session) {
+        Optional<UserEntity> optionalUser = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString()));
+
+        if (optionalUser.isPresent()) {
+            session.setAttribute("user_session_id", optionalUser.get().getId());
+            return "redirect:/post/home";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/login";
     }
 
 }
