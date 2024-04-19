5835f81a81a6389dc61b9fbe7e3fcec219eb6eec
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-19 13:45:40 +0100

    Create user controller, create login and register views

---
+++ personal-blog/src/main/java/com/jossegonnza/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.jossegonnza.controller;
+
+import com.jossegonnza.entity.UserEntity;
+import com.jossegonnza.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/record")
+    public String recordPage() {
+        return "/users/register";
+    }
+
+    @PostMapping("/register")
+    public String register(UserEntity user) {
+        userService.createUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = {"/login", "/"})
+    public String loginPage() {
+        return "/users/login";
+    }
+
+}
