d19cdf91878a56bea875a748c6ed31865fd43a18
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-20 15:18:55 +0100

    Added comments functionality

---
+++ personal-blog/src/main/java/com/jossegonnza/controller/CommentController.java
@@ -0,0 +1,45 @@
+package com.jossegonnza.controller;
+
+import com.jossegonnza.entity.CommentEntity;
+import com.jossegonnza.entity.PostEntity;
+import com.jossegonnza.entity.UserEntity;
+import com.jossegonnza.service.CommentService;
+import com.jossegonnza.service.PostService;
+import com.jossegonnza.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/addComment")
+    public String addComment(@RequestParam("postId") Long postId, CommentEntity comment, HttpSession session) {
+
+        UserEntity user = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString())).get();
+        PostEntity post = postService.getPostById(postId).orElseThrow(() -> new IllegalArgumentException("¡Id del post Inválido!"));
+
+        comment.setCreatedAt(LocalDateTime.now());
+        comment.setUser(user);
+        comment.setPost(post);
+
+        commentService.createComment(comment);
+        return "redirect:/post/postPage/" + postId;
+    }
+
+}
