070f8d3bff4c276d6d995989c610c53fa60d3cab
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-20 13:06:23 +0100

    Create endpoints and views of the post page and my posts

--- personal-blog/src/main/java/com/jossegonnza/controller/PostController.java
+++ personal-blog/src/main/java/com/jossegonnza/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.jossegonnza.controller;
 
+import com.jossegonnza.entity.CommentEntity;
 import com.jossegonnza.entity.PostEntity;
 import com.jossegonnza.entity.UserEntity;
 import com.jossegonnza.service.PostService;
@@ -10,10 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/post")
@@ -45,6 +48,26 @@ public class PostController {
 
         postService.createPost(post);
         return "redirect:/post/home";
+    }
+
+    @GetMapping("/postPage/{id}")
+    public String postPage(@PathVariable Long id, Model model) {
+        PostEntity post = postService.getPostById(id).orElseThrow(() -> new IllegalArgumentException("¡Id del posteo inválido!"));
+        List<CommentEntity> comments = post.getCommments();
+
+        model.addAttribute("post", post);
+        model.addAttribute("comments", comments);
+
+        return "/posts/post-page";
+    }
+
+    @GetMapping("/mine")
+    public String myPosts(Model model, HttpSession session) {
+        Long userId = Long.parseLong(session.getAttribute("user_session_id").toString());
+        List<PostEntity> posts = postService.getPostByUserId(userId);
+
+        model.addAttribute("posts", posts);
+        return "/posts/my-post";
     }
 
 }
